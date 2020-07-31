06aade89c206b663815122348740283ffb6ba955
fpatel3 <forampatel3010@gmail.com>
2020-07-31 02:42:43 -0400

    Added java script for Sign In page
    Made javascript for signinn page

---
+++ SIGNIN/script.js
@@ -0,0 +1,25 @@
+function validateForm(){
+    var email = document.getElementById('email');
+    var psw = document.getElementById('psw');
+    var psw_re = document.getElementById('psw-re');
+    var foo = false
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+    {
+        foo = true;
+    }
+    else{
+        foo = false
+        alert("You have entered an invalid email address!")
+        
+    }
+    foo = false;
+    if (psw.value == psw_re.value ){
+        foo = true
+    }
+    else{
+        alert("Password and confirm password must match")
+        foo = false
+    }
+
+    return foo;
+}
